c2a08c6e95a4c29b6ec3659e93af2ab404046712
jmurrah <jacobhmurrah@gmail.com>
2024-06-08 18:51:57 -0500

    Finish Day01 part 2 for python

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -1,7 +1,31 @@
+from collections import defaultdict
+
+
 def find_number(line: str):
-    for character in line:
-        if character.isdigit():
-            return character
+    digits = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    locations = defaultdict(str)
+
+    for i in range(len(line)):
+        for j in range(1, 6):
+            substring = line[i : i + j]
+            if substring in digits:
+                locations[i] = digits[substring]
+                break
+            elif substring.isdigit():
+                locations[i] = substring
+                break
+
+    return int(locations[min(locations)] + locations[max(locations)])
 
 
 if __name__ == "__main__":
@@ -11,6 +35,6 @@ if __name__ == "__main__":
 
     total = 0
     for line in lines:
-        total += int(find_number(line) + find_number(line[::-1]))
+        total += find_number(line)
 
     print(total)
