908d3bc3a511b489e519df65ecd4eb2b2d39d31e
jmurrah <jacobhmurrah@gmail.com>
2024-06-13 09:10:06 -0500

    Finish day03 part 1 in python

--- 2023/day03/day03.py
+++ 2023/day03/day03.py
@@ -1,46 +1,47 @@
-def get_numbers(data, indicies):
+def get_numbers(data, indicies) -> tuple[int, int, int]:
     numbers = []
 
     for index in indicies:
         i, j = index
-        while data[i][j] != ".":
+        while j >= 0 and j < len(data[i]) and data[i][j].isdigit():
             j -= 1
-
         j += 1
-        
+
         number, start = "", j
-        # print(data[i][j])
-        print(data[i])
-        print(i, j)
         while data[i][j].isdigit():
             number += data[i][j]
             j += 1
-        
-        numbers.append((start, i, int(number.strip())))
-    
+
+        numbers.append((start, i, int(number)))
+
     return numbers
 
 
-def get_adjacent_digit_indicies(data, x, y):
-    indicies = []
+def get_adjacent_digit_indicies(data, memory, x, y):
+    if (x, y) in memory:
+        return memory[(x, y)]
 
+    indicies = []
     for i in range(-1, 2):
         for j in range(-1, 2):
             if data[x + i][y + j].isdigit():
                 indicies.append((x + i, y + j))
 
+    memory[(x, y)] = indicies
     return indicies
 
 
 if __name__ == "__main__":
-    with open("input1.txt") as file:
+    with open("input.txt") as file:
         data = file.readlines()
 
-    numbers = []
+    numbers, memory = [], {}
+
     for i, line in enumerate(data):
         for j, character in enumerate(line.strip()):
             if not character.isdigit() and character != ".":
-                numbers.append(get_numbers(data, get_adjacent_digit_indicies(data, i, j)))
+                numbers.extend(
+                    get_numbers(data, get_adjacent_digit_indicies(data, memory, i, j))
+                )
 
     print(sum([number[-1] for number in set(numbers)]))
-    
